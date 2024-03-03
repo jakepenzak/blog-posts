af10843e1d73aed0e95bbeaf40319001304b465e
Jacob Pieniazek <j227487@8451.com>
2024-03-03 18:36:10 -0500

    Ruffed Up

--- Predictive Parameters in a Logistic Regression/marg_eff.py
+++ Predictive Parameters in a Logistic Regression/marg_eff.py
@@ -1,33 +1,32 @@
 import numpy as np
 import pandas as pd
 
+
 # Marginal Effects Function
-def logit_margeff(model, X, features, kind='probability'):
-    
+def logit_margeff(model, X, features, kind="probability"):
     coef = model.coef_
     intercept = model.intercept_
-    
-    if kind == 'probability':
-        
+
+    if kind == "probability":
         logodds = intercept + X @ coef.T
-    
-        marg_effects=[]
+
+        marg_effects = []
         for i in range(coef.size):
-            marg_eff = np.mean(coef[0,i]*np.exp(-logodds)/(1+np.exp(-logodds))**2).round(3)[0]
+            marg_eff = np.mean(
+                coef[0, i] * np.exp(-logodds) / (1 + np.exp(-logodds)) ** 2
+            ).round(3)[0]
             marg_effects.append(marg_eff)
-    
+
     elif kind == "odds":
-        
-        marg_effects=[]
+        marg_effects = []
         for i in range(coef.size):
-            marg_eff = (np.exp(coef[0,i])).round(3)
+            marg_eff = (np.exp(coef[0, i])).round(3)
             marg_effects.append(marg_eff)
-    
+
     marginal_effects = {}
-    marginal_effects['features'] = features
-    marginal_effects[f'marginal_effects_{kind}'] = marg_effects
+    marginal_effects["features"] = features
+    marginal_effects[f"marginal_effects_{kind}"] = marg_effects
 
     df = pd.DataFrame(marginal_effects)
 
     return df
-
